e088a34282b70bcb735b515bb58b82b4f6f558d4
Priyanka Jadhav <kadam.priyanka1@gmail.com>
2026-01-06 21:39:56 +0530

    init

---
+++ src/main/java/com/scaler/lld/bird/Bird.java
@@ -0,0 +1,12 @@
+package com.scaler.lld.bird;
+
+import lombok.Getter;
+
+@Getter
+public class Bird {
+    private String name;
+    private Double weight;
+    private  BirdType type;
+
+
+}
---
+++ src/main/java/com/scaler/lld/bird/BirdType.java
@@ -0,0 +1,9 @@
+package com.scaler.lld.bird;
+
+public enum BirdType {
+    EAGLE,
+    PEACOCK,
+    SPARROW,
+    DOVE,
+    PARROT
+}
